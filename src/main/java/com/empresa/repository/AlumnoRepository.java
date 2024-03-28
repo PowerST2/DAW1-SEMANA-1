a665c897f51ca1486fdd66129d86f9d7469db2a3
PowerST2 <diegostd99@gmail.com>
2024-03-27 20:31:13 -0500

    tarea

--- src/main/java/com/empresa/repository/AlumnoRepository.java
+++ src/main/java/com/empresa/repository/AlumnoRepository.java
@@ -8,8 +8,8 @@ import com.empresa.entity.Alumno;
 
 public interface AlumnoRepository extends JpaRepository<Alumno, Integer>{
 	
-
 	public List<Alumno> findByDni(String dni);
 	public List<Alumno> findByNombreLike(String nombre);
+	public List<Alumno> findByIdAlumno(int id);
 
 }
--- src/main/java/com/empresa/service/AlumnoService.java
+++ src/main/java/com/empresa/service/AlumnoService.java
@@ -5,8 +5,9 @@ import java.util.List;
 import com.empresa.entity.Alumno;
 
 public interface AlumnoService {
-
+	
 	public abstract List<Alumno> listaAlumno();
 	public abstract List<Alumno> listaAlumnoPorDni(String dni);
 	public abstract List<Alumno> listaAlumnoPorNombreLike(String nombre);
+	public abstract List<Alumno> listaAlumnoPorId(int idAlumno);
 }
